fb4b9503aade042f61ca3060ee85faa47d2f0731
naveen-06 <70632802+naveen-06@users.noreply.github.com>
2021-03-10 01:58:54 -0500

    Add files via upload

---
+++ precache-manifest.fda975811988d9667198cca8111a2d50.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b9b6c14151cd66c4a19a8c68c43235",
+    "url": "/color-picker/index.html"
+  },
+  {
+    "revision": "e2503d221ace75db1ffd",
+    "url": "/color-picker/static/css/main.0793bb44.chunk.css"
+  },
+  {
+    "revision": "85851a1d0812701979ab",
+    "url": "/color-picker/static/js/2.bf9d2882.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/color-picker/static/js/2.bf9d2882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2503d221ace75db1ffd",
+    "url": "/color-picker/static/js/main.d9521869.chunk.js"
+  },
+  {
+    "revision": "9f2f1b83fd3ea7a8bb90",
+    "url": "/color-picker/static/js/runtime-main.ac77659b.js"
+  },
+  {
+    "revision": "de275f93c2571e55796423064550c2e3",
+    "url": "/color-picker/static/media/Confetti-Doodles.de275f93.svg"
+  },
+  {
+    "revision": "1e71bac71dbb669f76025968bfe92ba4",
+    "url": "/color-picker/static/media/add.1e71bac7.svg"
+  },
+  {
+    "revision": "e3741d9b2ef1808d3e4cab1a1ab29ca8",
+    "url": "/color-picker/static/media/cancel.e3741d9b.svg"
+  },
+  {
+    "revision": "437d6e6bc04308e3b2d50591f8f98bd3",
+    "url": "/color-picker/static/media/logo.437d6e6b.svg"
+  },
+  {
+    "revision": "a450cc103b020589bf97577c53c009d7",
+    "url": "/color-picker/static/media/menu.a450cc10.svg"
+  },
+  {
+    "revision": "437d6e6bc04308e3b2d50591f8f98bd3",
+    "url": "/color-picker/static/media/paint-palette.437d6e6b.svg"
+  },
+  {
+    "revision": "d544872ed9d1ccdf6e78a632b71e7d8b",
+    "url": "/color-picker/static/media/random.d544872e.svg"
+  }
+]);
